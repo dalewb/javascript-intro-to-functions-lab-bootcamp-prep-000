cc7c3b54ec7ecd3bb251cd9315dcb401c2c0989a
William Dale <williambdale@gmail.com>
2018-02-07 15:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(str) {
 function sayHiToGrandma(str) {
   if (str === str.toLowerCase()) {
     return "I can't hear you!"
-} else if (str === str.toUpperCase() {
+} else if (str === str.toUpperCase()) {
     return "YES INDEED!"
-} else if str === "I love you, Grandma." {
-    return) "I love you too."
+} else if (str === "I love you, Grandma.") {
+    return "I love you too."
 }
 }
